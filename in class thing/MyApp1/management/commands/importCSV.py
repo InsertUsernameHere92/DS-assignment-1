8830a29ec10a0801c326162bfabefc6d56690d1b
InsertUsernameHere92 <0611640@schoolsnet.act.edu.au>
2026-03-16 17:03:05 +1100

    wip

--- in class thing/MyApp1/management/commands/importCSV.py
+++ in class thing/MyApp1/management/commands/importCSV.py
@@ -1,14 +1,25 @@
 import csv
 from django.core.management import BaseCommand
 from MyApp1.models import teacher
+from MyApp1.models import courses
 
 class Command(BaseCommand):
     help = "Help text not found... Sorry!"
+
     def add_arguments(self, parser):
         parser.add_argument('--path', type=str)
+        parser.add_argument('--model', type=str)
+
     def handle(self, *args, **kwargs):
         path = kwargs['path']
+        model = kwargs['model']
         with open(path, 'rt', encoding='utf-8-sig') as f:
             reader = csv.reader(f, dialect='excel')
-            for row in reader:
-                teacher.objects.create(Name=row[0], Area=row[1])
+            count = 0
+            if model:
+                for row in reader:
+                    model.objects.create(Name=row[0],Area=row[1])
+                    count=+1
+                print('Successfully imported ' + str(count) + ' new entries!')
+            else:
+                print('No model specified! Import failed')
